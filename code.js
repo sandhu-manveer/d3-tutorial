c80a458f564e41a56e91426364d1cffaf44cc306
sandhu-manveer <sandhu94manveer@gmail.com>
2017-11-29 17:00:38 +0530

    svg images

--- code.js
+++ code.js
@@ -1,51 +1,82 @@
-var Data;
+var svg = d3.select('#graph')
+.append('svg')
+.style('width', 1024)
+.style('height', 768);
 
-var table = d3.select('#graph')
-.append('table')
-.attr('class', 'table');
+svg.append('text')
+.text("A picture!")
+.attr({x: 10,
+  y: 150,
+    'text-anchor': 'start'});
 
-var thead = table.append('thead'),
-tbody = table.append('tbody');
+svg.append('line')
+    .attr({x1: 10,
+    y1: 10,
+    x2: 100,
+    y2: 100,
+    stroke: 'blue',
+    'stroke-width': 3});
 
-var reload = function () {
-    d3.csv('villains.csv', function (data) {
-      Data = data;
-      redraw();
-    });
-  };
-reload();
+svg.append('rect')
+    .attr({x: 200,
+      y: 50,
+      width: 300,
+      height: 400});
 
-var redraw = function () {
-    var tr = tbody.selectAll('tr')
-    .data(Data);
+svg.select('rect')
+    .attr({stroke: 'green',
+    'stroke-width': 0.5,
+    fill: 'white',
+    rx: 20,
+    ry: 40});
 
-    tr.enter()
-    .append('tr');
+svg.append('circle')
+    .attr({cx: 350,
+      cy: 250,
+      r: 100,
+      fill: 'green',
+      'fill-opacity': 0.5,
+      stroke: 'steelblue',
+      'stroke-width': 2});
 
-    tr.exit()
-    .remove();
-    
-    tr.selectAll('td')
-    .data(function (d) { return d3.values(d); })
-    .enter()
-    .append('td')
-    .text(function (d) { return d; });
-};
+svg.append('ellipse')
+    .attr({cx: 350,
+    cy: 250,
+    rx: 150,
+    ry: 70,
+    fill: 'green',
+    'fill-opacity': 0.3,
+    stroke: 'steelblue',
+    'stroke-width': 0.7});
 
-// arrange by appearance
-tbody.selectAll('tr')
-.sort(function (a, b) { return d3.ascending(a['Year first'], b['Year first']); });
-redraw();
+svg.append('ellipse')
+    .attr({cx: 350,
+      cy: 250,
+      rx: 20,
+      ry: 70});
+  
+svg.selectAll('ellipse, circle')
+    .attr('transform', 'translate(150, 0)');
 
-// required to make the list by doctor name work
-tbody.selectAll('tr').sort(function (a, b) {
-    return d3.descending(Number(a['Doc. no.']), Number(b['Doc. no.']));
-  });
+svg.selectAll('ellipse, circle')
+    .attr('transform', 'translate(150, 0) rotate(45)');
 
-// select only one doctor
-Data = Data.filter(function (d) { return d['Doctor actor'] == 'Matt Smith'; })
-redraw()
+svg.selectAll('ellipse, circle')
+    .attr('transform', 'translate(150, 0) rotate(-45, 350, 250)');
 
-tbody.selectAll('tr')
-.filter(function (d) { return d['Doctor actor'] != 'Matt Smith'; })
-.remove()
+svg.selectAll('ellipse, circle')
+    .attr('transform', 'translate(150, 0) rotate(-45, 350, 250) scale(1.2)');
+
+svg.selectAll('ellipse, circle')
+    .attr('transform', 'translate(150, 0) scale(1.2) translate(-70, 0) rotate(-45, '+(350/1.2)+', '+(250/1.2)+')');
+
+svg.selectAll('ellipse, circle')
+    .attr('transform', 'translate(150, 0) scale(1.2) translate(-70, 0) rotate(-45, '+(350/1.2)+', '+(250/1.2)+') skewY(20)');
+
+// https://www.w3.org/TR/SVG/coords.html#EstablishingANewUserSpace
+svg.append('path')
+.attr({d: 'M 100 100 L 300 100 L 200 300 z',
+  stroke: 'black',
+  'stroke-width': 2,
+  fill: 'red',
+  'fill-opacity': 0.7});
