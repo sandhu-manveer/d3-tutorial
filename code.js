8ebba6b787b2fb0349c902c68239473f248f49a9
sandhu-manveer <sandhu94manveer@gmail.com>
2017-11-30 14:29:22 +0530

    colour wheel

--- code.js
+++ code.js
@@ -1,39 +1,38 @@
-var width = 800,
-height = 600,
-margin = 20,
-svg = d3.select('#graph')
-  .append('svg')
-  .attr({width: width,
+var width = 1024,
+height = 768,
+rings = 15;
+
+var svg = d3.select('#graph')
+    .append('svg')
+    .style({width: width,
                 height: height});
 
-var x = d3.scale.linear().domain([0, 100]).range([margin, width-margin]);
+var colors = d3.scale.category20b();
+var angle = d3.scale.linear().domain([0, 20]).range([0, 2*Math.PI]);
 
-var axes = [
-    d3.svg.axis().scale(x),
-  
-    d3.svg.axis().scale(x)
-      .ticks(5),
-    
-    d3.svg.axis().scale(x)
-      .tickSubdivide(3)
-      .tickSize(10, 5, 10),
+var arc = d3.svg.arc()
+.innerRadius(function (d) { return d*50/rings; })
+.outerRadius(function (d) { return 50+d*50/rings; })
+.startAngle(function (d, i, j) { return angle(j); })
+.endAngle(function (d, i, j) { return angle(j+1); });
 
-    d3.svg.axis().scale(x)
-      .tickValues([0, 20, 50, 70, 100])
-      .tickFormat(function (d, i) {
-        return ['a', 'e', 'i', 'o', 'u'][i];
-      })
-      .orient('top')
-  ];
+var shade = {
+    darker: function (d, j) { return d3.rgb(colors(j)).darker(d/rings); },
+    brighter: function (d, j) { return d3.rgb(colors(j)).brighter(d/rings); }
+};
 
-  axes.forEach(function (axis, i) {
-    var a = svg.append('g')
-        .classed('axis', true)
-        .classed('red', i%2 == 0)
-        .attr('transform', 'translate(0, '+(i*50+margin)+')')
-        .data(d3.range(0, 100))
-        .call(axis);
-  });
-
-var axis = d3.svg.axis()
-.scale(x);
+[[100, 100, shade.darker],
+[300, 100, shade.brighter]].forEach(function (conf) {
+ svg.append('g')
+   .attr('transform', 'translate('+conf[0]+', '+conf[1]+')')
+   .selectAll('g')
+   .data(colors.range())
+   .enter()
+   .append('g')
+   .selectAll('path')
+   .data(function (d) { return d3.range(0, rings); })
+   .enter()
+   .append('path')
+   .attr("d", arc)
+   .attr('fill', function (d, i, j) { return conf[2](d, j); });
+});
